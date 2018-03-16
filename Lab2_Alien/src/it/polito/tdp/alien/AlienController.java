9b949d02c0f0547b9918063afbb94d71c3d180e2
Ludovica Leonardo <s226758@studenti.polito.it>
2018-03-16 15:36:18 +0100

    versione 1

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -4,10 +4,9 @@ package it.polito.tdp.alien;
  * Sample Skeleton for 'Alien.fxml' Controller Class
  */
 
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,40 +15,61 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	
-    @FXML
-    private ResourceBundle resources;
-    @FXML
-    private URL location;
-    @FXML
-    private TextField txtWord;
-    @FXML
-    private TextArea txtResult;
-    @FXML
-    private Button btnTranslate;
-    @FXML
-    private Button btnReset;
-        
-    
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-    	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
-    	
-    }
-  
-    
-    @FXML
-    void doTranslate(ActionEvent event) {
-    	    	
-    }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
+	AlienDictionary alien;
 
-    }
-    
+	@FXML
+	private ResourceBundle resources;
+	@FXML
+	private URL location;
+	@FXML
+	private TextField txtWord;
+	@FXML
+	private TextArea txtResult;
+	@FXML
+	private Button btnTranslate;
+	@FXML
+	private Button btnReset;
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
+
+		alien = new AlienDictionary();
+	}
+
+	@FXML
+	void doTranslate(ActionEvent event) {
+		String parole[];
+		String parola = txtWord.getText();
+		parola=parola.trim();
+		txtWord.clear();
+		if (parola.matches("(^)([ ]*)([a-zA-Z]+)([ ]+)([a-zA-Z]+)([ ]*)($)")) {
+		if (parola.contains(" ")) {
+			parole = parola.split(" ");
+			if (parole.length > 1) {
+				alien.addWord(parole[0], parole[1]);
+				txtResult.setText("Aggiunta nuova Parola");
+				}
+			}
+		}
+	 else {
+		 if (parola.matches("(^)([ ]*)([a-zA-Z]+)([ ]*)($)")){
+				txtWord.clear();
+			txtResult.setText(alien.translateWord(parola));
+	 		}
+	 }
+		//txtResult.setText("Devi inserire solo caratteri!");
+		}
+	
+
+	@FXML
+	void doReset(ActionEvent event) {
+		txtResult.clear();
+		alien.dizionario.clear();
+
+	}
+
 }
--- Lab2_Alien/src/it/polito/tdp/alien/Test.java
+++ Lab2_Alien/src/it/polito/tdp/alien/Test.java
@@ -1,5 +1,14 @@
 package it.polito.tdp.alien;
 
-public class Test {
 
+public class Test {
+	
+	public static void main(String[] args) {
+	
+	AlienDictionary aliDic = new AlienDictionary();
+	
+	aliDic.addWord("asdasd","asdasda");
+	aliDic.addWord("ciao", "oaic");
+	
+	}
 }
